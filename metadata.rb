7bcfed6d6ce7be62aa932a23f23e6df1e63fa987
Nick Chernyshev <nick.chernyshev@gmail.com>
2014-03-17 15:03:00 +0600

    bump new version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "chef-nginx"
 maintainer        "Dwwd"
 maintainer_email  "info@dwwd.ru"
 description       "Installs the nginx web server from the nginx ppa"
-version           "0.0.1"
+version           "0.0.2"
 
 recipe "chef-nginx", "nginx server"
 
