7e66f92b61da72dae227eccea2844478f4d26ad8
Nick Chernyshev <nick.chernyshev@gmail.com>
2014-12-11 13:31:45 +0500

    add main recipe conditions

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,4 @@
 default[:nginx][:run_user] = 'www-data'
+default[:nginx][:reconfigure] = true
+default[:nginx][:update_ufw_rules] = true
+default[:nginx][:use_stable_ppa] = true
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,14 +2,16 @@
 # tends to be at least one major version behind
 
 # need this for add-apt-repository to work properly
-package 'python-software-properties'
+if node[:nginx][:use_stable_ppa]
+  package 'python-software-properties'
 
-bash 'adding stable nginx ppa' do
-  user 'root'
-  code <<-EOC
-    add-apt-repository ppa:nginx/stable
-    apt-get update
-  EOC
+  bash 'adding stable nginx ppa' do
+    user 'root'
+    code <<-EOC
+      add-apt-repository ppa:nginx/stable
+      apt-get update
+    EOC
+  end
 end
 
 # install nginx
@@ -18,29 +20,38 @@ package "nginx"
 # create the new main nginx config file
 # do as little as possible in here, most 
 # of this should be configured per site
-template "/etc/nginx/nginx.conf" do
-  owner "root"
-  group "root"
-  mode "0644"
-  source "nginx.conf.erb"
-  notifies :run, "execute[restart-nginx]", :immediately
+if node[:nginx][:reconfigure]
+  template "/etc/nginx/nginx.conf" do
+    owner "root"
+    group "root"
+    mode "0644"
+    source "nginx.conf.erb"
+    notifies :run, "execute[restart-nginx]", :immediately
+  end
 end
 
 # rails app upstream
 if node[:nginx][:upstream]
-  template "/etc/nginx/sites-enabled/#{node[:nginx][:upstream][:server_name]}" do
+  template "/etc/nginx/sites-available/#{node[:nginx][:upstream][:server_name]}" do
     owner "#{node[:nginx][:upstream][:run_user]}"
     group "#{node[:nginx][:upstream][:run_user]}"
     mode "0644"
     source "upstream.erb"
     notifies :run, "execute[restart-nginx]", :immediately
   end
+
+  bash "symlink available site" do
+    user "root"
+    code "ln -s /etc/nginx/sites-available/#{node[:nginx][:upstream][:server_name]} /etc/nginx/sites-enabled/#{node[:nginx][:upstream][:server_name]}"
+  end
 end
 
-# open port 80
-bash "allowing nginx traffic through firewall" do
-  user "root"
-  code "ufw allow 80 && ufw allow 443"
+# open web ports
+if node[:nginx][:update_ufw_rules]
+  bash "allowing nginx traffic through firewall" do
+    user "root"
+    code "ufw allow 80 && ufw allow 443"
+  end
 end
 
 execute "restart-nginx" do
